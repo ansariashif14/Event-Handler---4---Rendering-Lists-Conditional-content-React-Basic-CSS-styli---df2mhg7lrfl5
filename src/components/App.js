c5cf2cca3be56e861c36afceccbc836c25b15070
ansariashif14 <ansariashif14@gmail.com>
2022-11-28 20:55:11 +0530

    half completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+    if(event.target.value)
+    {
+      console.lof("I was double clicked");
+    }
+   else {
+      console.log("I was not double clicked");
+    }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button onDoubleClick={handleDoubleClick} id="dblclick-btn">Double click me</button>
     </div>
   )
 }
